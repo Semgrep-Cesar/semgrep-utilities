7e8e0260b29c629d097cf2ed2ff41c4dd28e9d49
Semgrep-Cesar <136354883+Semgrep-Cesar@users.noreply.github.com>
2023-07-24 11:07:16 -0400

    Update schemaDependency.py

--- integrations/gitlab/schemaDependency.py
+++ integrations/gitlab/schemaDependency.py
@@ -1,98 +1,138 @@
 import json
 import argparse
+import re
 
-def modify_schema(string_to_add, new_type_value, description):
+def modify_schema(string_to_add, packageName, fingerprint, packageType, description, properties, ruleId, version):
     schema = {
-         "$schema": "http://json-schema.org/draft-07/schema#",
-        "$id": "https://gitlab.com/gitlab-org/security-products/security-report-schemas/-/raw/master/dist/dependency-scanning-report-format.json",
-        "title": "Report format for GitLab Dependency Scanning",
-        "description": "This schema provides the the report format for Dependency Scanning analyzers (https://docs.gitlab.com/ee/user/application_security/dependency_scanning).",
-        "allOf": [
-            { "$ref": "security-report-format.json" },
-            {
-                "properties": {
-                    "scan": {
-                        "properties": {
-                            "type": {
-                                "enum": ["dependency_scanning"]
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "properties": {
-                    "dependency_files": {
-                        "type": "array",
-                        "description": "List of dependency files identified in the project.",
-                        "items": {
-                            "$ref": "security-report-format.json#/definitions/dependency_file"
-                        }
-                    }
-                },
-                "required": [ "dependency_files" ]
-            },
-            {
-                "properties": {
-                    "vulnerabilities": {
-                        "items": {
-                            "properties": {
-                                "location": { "$ref": "#/definitions/location" }
-                            },
-                            "required": [ "location" ]
-                        }
-                    }
-                }
-            }
-        ],
-        "definitions": {
+        "version": "15.0.0",
+        "vulnerabilities": [
+        {
+            "id": "0e8294b7f5226e2e75d8369e100cd610face57ef67bc1af625ddb250785726a7",
+            "name": "Deserialization of Untrusted Data Semgrep",
+            "description": "In Apache Log4j.",
+            "cve": "",
+            "severity": "Critical",
+            "solution": "Please review Semgrep SCA rule",
             "location": {
-                "type": "object",
-                "description": "Identifies the vulnerability's location.",
-                "properties": {
-                    "file": {
-                        "type": "string",
-                        "minLength": 1,
-                        "description": "Path to the manifest or lock file where the dependency is declared (such as yarn.lock)."
-                    },
-                    "dependency": {
-                        "$ref": "security-report-format.json#/definitions/dependency"
-                    }
+            "dependency_files": "pom.xml",
+            "dependency": {
+                "package": {
+                "name": "org.apache.logging.log4j/log4j-core"
                 },
-                "required": [ "file", "dependency" ]
+                "version": "2.6.1"
             }
+            },
+            "identifiers": [
+            {
+                "type": "gemnasium",
+                "name": "Gemnasium-ef60b3d6-926c-472f-b24a-f585deccf8b6",
+                "value": "ef60b3d6-926c-472f-b24a-f585deccf8b6",
+                "url": "https://gitlab.com/gitlab-org/security-products/gemnasium-db/-/blob/master/maven/org.apache.logging.log4j/log4j-core/CVE-2017-5645.yml"
+            },
+            {
+                "type": "cve",
+                "name": "CVE-2017-5645",
+                "value": "CVE-2017-5645",
+                "url": "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-5645"
+            },
+            {
+                "type": "ghsa",
+                "name": "GHSA-fxph-q3j8-mv87",
+                "value": "GHSA-fxph-q3j8-mv87",
+                "url": "https://github.com/advisories/GHSA-fxph-q3j8-mv87"
+            }
+            ],
+            "links": [
+            {
+                "url": "http://www.openwall.com/lists/oss-security/2019/12/19/2"
+            }
+            ],
+            "details": {
+            "vulnerable_package": {
+                "type": "text",
+                "name": "Vulnerable Package",
+                "value": "org.apache.logging.log4j/log4j-core:2.6.1"
+            }
+            }
+        }
+        ],
+        "scan": {
+        "analyzer": {
+            "id": "gemnasium-maven",
+            "name": "Semgrep",
+            "url": "https://gitlab.com/gitlab-org/security-products/analyzers/gemnasium",
+            "vendor": {
+            "name": "GitLab"
+            },
+            "version": "4.1.0"
+        },
+        "scanner": {
+            "id": "gemnasium-maven",
+            "name": "Semgrep",
+            "url": "https://gitlab.com/gitlab-org/security-products/analyzers/gemnasium",
+            "vendor": {
+            "name": "GitLab"
+            },
+            "version": "4.1.0"
+        },
+        "type": "dependency_scanning",
+        "start_time": "2023-07-20T14:54:46",
+        "end_time": "2023-07-20T14:55:01",
+        "status": "success"
         }
     }
 
-    def add_value_to_required_key(schema_dict, string_to_add, new_type_value, description, indent=0):
+    def add_value_to_required_key(schema_dict, string_to_add, packageName, fingerprint, packageType, description, properties, ruleId, version, indent=0):
         for key, value in schema_dict.items():
-            if key == 'required' and 'dependency_files' in value:
-                value.append(string_to_add)
             
-            # Check for 'type' keys with a string value and set to new_type_value
-            if key == 'type' and isinstance(value, str):
-                schema_dict[key] = new_type_value
+            if key == 'id' and '0e8294b7f5226e2e75d8369e100cd610face57ef67bc1af625ddb250785726a7' in value:
+                schema_dict[key] = fingerprint
+
+            if key == 'name' and 'Deserialization of Untrusted Data Semgrep' in value:
+                schema_dict[key] = packageName.replace('\"', '').replace(':', '').replace(' ', '').replace('{', '')
+            
+            # Check for 'type' keys with a string value and set to packageType
+            if key == 'description' and 'In Apache Log4j.' in value:
+                schema_dict[key] = description
             
             #Check for 'type' key with a string value and set to description
             if key == 'description' and isinstance(value, str) and "Identifies the vulnerability's location." in value:
                 schema_dict[key] = value.replace("Identifies the vulnerability's location.", description)
             
             #Check for 'type' key with a string value and set to reachable
+            if key == 'severity' and 'Critical' in value and properties == 'reachable':
+                schema_dict[key] = 'Critical'
+            elif key == 'severity' and 'Critical' in value and properties != 'reachable':
+                schema_dict[key] = 'High'
+            
+            #Append rule to text in solution
+            if key == 'solution' and 'Please review Semgrep SCA rule' in value:
+                textInSolution = [value, ruleId]
+                joinedText = ' '.join(textInSolution)
+                schema_dict[key] = joinedText
+            
+            #Change the value of the dependency file
+            if key == 'dependency_files' and 'pom.xml' in value:
+                schema_dict[key] = string_to_add
+            
+            #change the package name 
+            if key == 'name' and 'org.apache.logging.log4j/log4j-core' in value:
+                schema_dict[key] = string_to_add
+            
+            if key == 'version' and '2.6.1' in value:
+                schema_dict[key] = version
 
             if isinstance(value, dict):
-                add_value_to_required_key(value, string_to_add, new_type_value, description, indent+1)
+                add_value_to_required_key(value, string_to_add, packageName, fingerprint, packageType, description, properties, ruleId, version, indent+1)
             elif isinstance(value, list):
                 for index, item in enumerate(value):
                     if isinstance(item, dict):
-                        add_value_to_required_key(item, string_to_add, new_type_value, description, indent+1)
-    add_value_to_required_key(schema, string_to_add, new_type_value, description)
+                        add_value_to_required_key(item, string_to_add, packageName, fingerprint, packageType, description, properties, ruleId, version, indent+1)
+    add_value_to_required_key(schema, string_to_add, packageName, fingerprint, packageType, description, properties, ruleId, version)
 
     # Save the modified schema to a file
     with open('gl-dependency-scanning-report.json', 'w') as f:
         json.dump(schema, f, indent=4)  # pretty print JSON
-
-import argparse
-import json
 
 def parse_sarif_file(filename):
     # Read and parse the JSON from a file
@@ -106,15 +146,53 @@ def parse_sarif_file(filename):
         print()
 
         for result in run['results']:
-            print('Fingerprints:', result['fingerprints'])
+            print('Fingerprints:', result['fingerprints']['matchBasedId/v1'])
             print('Location URI:', result['locations'][0]['physicalLocation']['artifactLocation']['uri'])
+            print('Location Base_Id:', result['locations'][0]['physicalLocation']['artifactLocation']['uriBaseId'])
+            print('Region End Column:', result['locations'][0]['physicalLocation']['region']['endColumn'])
+            print('Region End Line:', result['locations'][0]['physicalLocation']['region']['endLine'])
+            print('Start Column:', result['locations'][0]['physicalLocation']['region']['startColumn'])
+            print('Start Column:', result['locations'][0]['physicalLocation']['region']['startLine'])
+            # print('Name:', result['locations'][0]['physicalLocation']['region']['snippet']['text'])
+            text = result['locations'][0]['physicalLocation']['region']['snippet']['text']
+            packageName = text.split("\n")[0].strip()
+            print('Name:', packageName)
+            version = None
+            match = re.search(r'"version":\s+"([^"]+)"', text)
+            if match:
+                version = match.group(1)
+                print('Version:', version)
+            else:
+                print('Version not found in text')
             print('Message:', result['message']['text'])
-            print('Properties:', result['properties'])
+            print('Properties:', result['properties']['exposure'])
             print('RuleId:', result['ruleId'])
             print()
+
+            #Values froms the semgrep supply chain ci output
+            fingerprint = result['fingerprints']['matchBasedId/v1']
+            locationUri = result['locations'][0]['physicalLocation']['artifactLocation']['uri']
+            locationBaseId = result['locations'][0]['physicalLocation']['artifactLocation']['uriBaseId']
+
+            #Regions and start columns are not used in this iteration
+            regionEndColumn = result['locations'][0]['physicalLocation']['region']['endColumn']
+            regionEndLine = result['locations'][0]['physicalLocation']['region']['endLine']
+            startColumn = result['locations'][0]['physicalLocation']['region']['startColumn']
+            startLine = result['locations'][0]['physicalLocation']['region']['startLine']
+            message = result['message']['text']
+            properties = result['properties']['exposure']
+            ruleId = result['ruleId']
+
             description = result['message']['text']
-            new_type_value = result['locations'][0]['physicalLocation']['artifactLocation']['uri']
-            modify_schema(result['locations'][0]['physicalLocation']['artifactLocation']['uri'], new_type_value, description)
+            packageType = str(result['locations'][0]['physicalLocation']['artifactLocation']['uri'])
+            modify_schema(locationUri, packageName, fingerprint, packageType, description, properties, ruleId, version)
+
+
+# Ask the user for the file name
+#filename = input("Please enter the file name: ")
+
+# Use the function
+#parse_sarif_file(filename)
 
 # Create the parser and add argument
 parser = argparse.ArgumentParser()
@@ -125,7 +203,5 @@ args = parser.parse_args()
 
 # Use the function with the filename as an argument
 parse_sarif_file(args.filename)
-
-
 
 
